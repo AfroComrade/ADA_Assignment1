3be4c5314aeb6119a7ccd0d47cadc0d11d20dbe6
JamesStratford <james.e.stratford@gmail.com>
2022-08-06 22:38:36 +1200

    Overhauled threadpool, task
    Bit rusty and needs a good polish but think it is working!!

--- ADA_Assignment1/src/ada_assignment1/ADA_Assignment1.java
+++ ADA_Assignment1/src/ada_assignment1/ADA_Assignment1.java
@@ -1,11 +1,39 @@
 package ada_assignment1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ADA_Assignment1
 {
 
     public static void main(String[] args)
     {
-
+        ThreadPool.get();
+        ThreadPool.get().performTask(new Task(0)
+        {
+            
+            
+            @Override
+            public void run()
+            {
+                addListener(new ConcreteObsever());
+                
+                for (int i =0; i < 100; ++i)
+                {
+                    try
+                    {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex)
+                    {
+                        Logger.getLogger(ADA_Assignment1.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    System.out.println(i);
+                    notifyAll(i);
+                }
+            }
+        });
+        
+        
     }
     
 }
